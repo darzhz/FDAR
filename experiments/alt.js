e36e36f72493ad2a49f0ea6060cd77ba299bae52
Darsh shyam kumar <68496521+darzhz@users.noreply.github.com>
2023-05-10 14:14:10 +0530

    passing and 3 memory leaks fixed

--- experiments/alt.js
+++ experiments/alt.js
@@ -1,31 +1,29 @@
 const express = require("express");
 const ffmpeg = require('fluent-ffmpeg');
 const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-backend-wasm');
 const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 const io = require('socket.io')(server);
-const Jimp = require('jimp');
+const poseDetection = require('@tensorflow-models/pose-detection');
+const MemoryStream = require('memorystream');
+const buffersize  = 460800;//921600;
 app.use(express.static('./'));
 async function loadModel() {
-  const modelUrl = 'http://localhost:3001/model.json'; 
-  const model = await tf.loadGraphModel(modelUrl);
+  // const modelUrl = 'http://localhost:3001/model.json';
+  // const model = await tf.loadGraphModel(modelUrl);
+  const model = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet,{
+        modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+        minPoseScore:0.1
+        });
   console.log("model Loaded");
   return model;
 }
-const poseEstimation = async (model,rtspUrl) => {
+const poseEstimation = async (model,rtspUrl,width,height) => {
 
   io.on('connect', (socket) => {
     console.log(`Client ${socket.id} connected`);
-    
-    // // Create an FFMPEG input stream
-    // const inputStream = ffmpeg(rtspUrl).inputOptions(['-rtsp_transport', 'tcp']);
-
-    // // Create an FFMPEG output stream
-    // const outputStream = inputStream.outputOptions([
-    // '-f', 'image2pipe',
-    // '-pix_fmt', 'rgb24',
-    // '-vcodec', 'rawvideo']);
 
     const outputStream = ffmpeg(rtspUrl)
   .inputOptions(['-rtsp_transport','tcp'])
@@ -39,51 +37,26 @@ const poseEstimation = async (model,rtspUrl) => {
   .outputOptions([
     '-f', 'image2pipe',
     '-pix_fmt', 'rgb24',
-    '-s' , '640x480',
-    '-vcodec', 'rawvideo','-vf', 'scale=640:480'])
+    '-vcodec', 'rawvideo','-bufsize',460800])
   .pipe();
 
     // Set up the output stream to pipe its data to the pose estimation function
-    let frameCount = 0;
+    let frameData = Buffer.alloc(0);
     outputStream.on('data', async (data) => {
-      // Convert the data buffer to a Uint8Array
+      if (frameData === null) { // Check if frameData is null before concatenating it with data
+      frameData = Buffer.alloc(0);
+      }
+      frameData = Buffer.concat([frameData, data]);
        console.log('Received data chunk: ' + data.length);
-       //const frame = Buffer.from(data);
-      const frame = new Uint8Array(data);
-      // const resizedImage = await Jimp.read(frame)
-      // .then((image) => image.resize(640, 480));
-      //.then((image) => image.getBufferAsync(Jimp.MIME_JPEG));
-
-
-      // Prepare the input tensor
-      // const imageTensor = tf.tensor3d(resizedImage, [480, 640, 3], 'int32');
-      // const inputTensor = imageTensor.expandDims();
-
-      // // Run the model inference
-      // const outputTensor = model.execute(inputTensor);
-      // const predictions = await outputTensor.array();
-
-      // // Convert the output to a more readable format
-      // const poseResults = predictions.map((pose) => ({
-      //   score: pose[0],
-      //   keypoints: pose.slice(1).map((keypoint) => ({
-      //     name: keypoint[0],
-      //     position: [keypoint[1], keypoint[2]],
-      //     score: keypoint[3]
-      //   }))
-      // }));
-
-      // Send the pose estimation results to the client
-      socket.emit('pose', frame);
-
-      // Log the pose estimation results
-      console.log(`Frame ${frameCount}: ${JSON.stringify(data.length)}`);
-      frameCount++;
+       if (frameData && frameData.length >= buffersize) {
+          if(frameData.length == buffersize){
+              const imageTensor = tf.tensor3d(frameData, [height, width, 3], 'int32');
+              const predictions = await model.estimatePoses(imageTensor);
+                socket.emit('pose', [predictions[0],frameData]);
+              }
+            frameData = frameData.slice(buffersize)
+        }
     });
-
-    // Start the FFMPEG process
-    //outputStream.run();
-
     // Handle errors from the FFMPEG process
     outputStream.on('error', (err, stdout, stderr) => {
       console.error(`FFMPEG error: ${err}`);
@@ -99,9 +72,13 @@ const poseEstimation = async (model,rtspUrl) => {
   });
 };
 //poseEstimation('rtsp://192.168.1.5:8080/h264_ulaw.sdp')
+tf.setBackend('wasm').then(() => main());
+function main(){
+console.log('wasm loaded');
 loadModel().then((model) => {
-  poseEstimation(model,'rtsp://192.168.1.3:8080/h264_ulaw.sdp');
+  poseEstimation(model,'rtsp://192.168.1.3:8080/h264_ulaw.sdp',480,320);
 })
+}
 const port = 3001;
   server.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
